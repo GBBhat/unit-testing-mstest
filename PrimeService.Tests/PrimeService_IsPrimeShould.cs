4663b7a4582a82f5caac8797b01f1999c86b9e16
gayabhat <gayatri.bhat@agilent.com>
2025-01-28 17:44:05 +0100

    Add project files.

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,34 @@
+﻿namespace PrimeService.Tests
+{
+    [TestClass]
+    public sealed class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, $"1 should not be prime");
+        }
+
+        #region Sample_TestCode
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+        #endregion
+    }
+}
